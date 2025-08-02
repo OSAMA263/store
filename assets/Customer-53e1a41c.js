2d16027ab468eea594c24ab485d204d398b50392
OSAMA263 <OSAMA263@users.noreply.github.com>
2025-08-02 05:27:57 +0000

    deploy: 756ba760a984a149d3d196f1e24dca21c430ac06

---
+++ assets/Customer-53e1a41c.js
@@ -0,0 +1,7 @@
+import{j as e,N as r,C as o,F as a}from"./index-b1778874.js";import{P as n}from"./PageHero-b2c2d557.js";import{F as s}from"./Form-70f90d7c.js";import"./index-36ef8543.js";import"./chunk-2ZHRCML3-8df56ec8.js";function h(){return e.jsxs(r,{children:[e.jsx(n,{title:"Customer Login",children:e.jsx("h1",{className:"text-lightGray",children:"Register & Login"})}),e.jsx(o,{children:e.jsxs(p,{children:[e.jsx(i,{}),e.jsx(m,{})]})})]})}const i=()=>{const t={bg:"bg-[#f7f7f7]",title:"Login",text:"Good to have you back!",btn:"LOGIN"};return e.jsxs(s,{...t,children:[e.jsx("input",{name:"name_email",type:"text",placeholder:"Username or email address"}),e.jsx("input",{name:"password",type:"password",placeholder:"Password"})]})},m=()=>{const t={bg:"",title:"Register",text:"If you don’t have an account, register now!",btn:"CREATE ACCOUNT"};return e.jsxs(s,{...t,children:[e.jsx("input",{name:"name",type:"text",placeholder:"Username"}),e.jsx("input",{name:"email",type:"text",placeholder:"Email address"}),e.jsx("input",{name:"password",type:"password",placeholder:"Password"})]})},p=a.div`
+grid
+lg:grid-cols-2
+gap-y-8
+lg:gap-x-20
+gap-x-3
+`;export{h as default};
--- assets/index-ce5ae199.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-28aa2f6c.js";function a(t){return r.Children.toArray(t).filter(e=>r.isValidElement(e))}export{a as g};
